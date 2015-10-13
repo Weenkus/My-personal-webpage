5feb108e4dbb16523e6684a1f47c5a20f57747db
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2015-10-13 21:52:26 +0200

    Users can log in and while loged in they can see their account informations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Weenkus blog site!"
       redirect_to @user
     else
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,4 +11,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+  
 end
